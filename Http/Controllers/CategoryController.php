9da031691e7786e5b25a81eebad8b6dd65086738
Kishan Baraiya <kishanbaraiya2002@gmail.com>
2024-01-19 15:56:34 +0530

    added

---
+++ Http/Controllers/CategoryController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+Use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        // $categories = Category::all();
+        // return view('categories.index', compact('categories'));
+        $categories = Category::all();
+        $categoryTree = $this->CategoryTree($categories);
+        return view('categories/index', ['categories' => $categoryTree]);
+    }
+    public function CategoryTree($categories, $parentId = 0)
+{
+    $categoryTree = [];
+
+    foreach ($categories as $category) {
+        if ($category->category_id == $parentId) {
+            $subcategory = $this->CategoryTree($categories, $category->id);
+            if ($subcategory) {
+                $category->setAttribute('children', $subcategory);
+            }
+            $categoryTree[] = $category;
+        }
+    }
+    return $categoryTree;
+}
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        // if ($request->hasFile('image')) {
+        //     $data['image'] = $request->file('image')->store('category_images', 'public');
+        // }
+        // Category::create($data);
+        // return redirect()->route('categories.index')->with('success', 'Category created successfully.');
+        $imagename = time(). '.' .$request->image->extension();
+        $request->image->move(public_path('products'),$imagename);
+        $category = new category();
+        $category->name = $request->name;
+        $category->image = $imagename;
+        // $category->slug = (str::slug($request->slug));
+        $category->save();
+        return back()->withSuccess('Category Added.....');
+        
+    }
+    public function allCategories()
+    {
+        $categories = Category::with('subcategories')->get();
+
+        return view('categories/allcategories', compact('categories'));
+    }
+    
+
+   
+}
---
+++ Http/Controllers/SubcategoryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Subcategory;
+use App\Models\Category;
+use Illuminate\Support\Facades\DB;
+
+class SubcategoryController extends Controller
+{
+    public function index()
+    {
+        $subcategories = Subcategory::all();
+        return view('subcategories.index', compact('subcategories'));
+    }
+
+    public function create()
+    {
+        $categories = Category::all();
+        return view('subcategories.create', compact('categories'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'category_id' => 'required|exists:categories,id',
+            'name' => 'required|string|max:255',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+        // if ($request->hasFile('image')) {
+        //     $data['image'] = $request->file('image')->store('subcategory_images', 'public');
+        // }
+
+        // Subcategory::create($data);
+
+        // return redirect()->route('subcategories.index')->with('success', 'Subcategory created successfully.');
+        $imagename = time(). '.' .$request->image->extension();
+        $request->image->move(public_path('products'),$imagename);
+        $category = new subcategory();
+        $category->category_id = $request->category_id;
+        $category->name = $request->name;
+        $category->image = $imagename;
+        // $category->slug = (str::slug($request->slug));
+        $category->save();
+        return back()->withSuccess('SubCategory Added.....');
+    }
+    public function view($id){
+         
+            $subcategory = DB::table('subcategories')->where('id',$id)->get();
+             return view('subcategories/view',['subcategories'=>$subcategory]);
+     
+    }
+
+   
+}
